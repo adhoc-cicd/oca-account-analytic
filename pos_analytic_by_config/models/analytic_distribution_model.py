OCA/account-analytic

[MIG] pos_analytic_by_config: Migration to 17.0

pos_analytic_by_config/models/analytic_distribution_model.py
@@ -13,8 +13,9 @@ class AccountAnalyticDistributionModel(models.Model):
     )
 
     def _get_distribution(self, vals):
+        new_context = vals.copy()
         pos_config_id = self.env.context.get("pos_config_id")
         if pos_config_id:
-            vals["pos_config_id"] = pos_config_id
-        res = super()._get_distribution(vals)
+            new_context["pos_config_id"] = pos_config_id
+        res = super()._get_distribution(new_context)
         return res

adhoc-cicd/oca-account-analytic

pos_analytic_by_config/models/analytic_distribution_model.py
@@ -13,6 +13,7 @@ class AccountAnalyticDistributionModel(models.Model):
     )
 
     def _get_distribution(self, vals):
+        vals = vals.copy()
         pos_config_id = self.env.context.get("pos_config_id")
         if pos_config_id:
             vals["pos_config_id"] = pos_config_id