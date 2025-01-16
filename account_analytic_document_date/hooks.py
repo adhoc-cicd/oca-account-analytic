OCA/account-analytic

[16.0][ADD] account_analytic_document_date: New module account_analytic_document_date

account_analytic_document_date/hooks.py
@@ -1,0 +1,12 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+def post_init_hook(cr, registry):
+    cr.execute(
+        """
+        UPDATE account_move
+        SET document_date = invoice_date
+        WHERE document_date IS NULL
+    """
+    )

account_analytic_document_date/models/account_move.py
@@ -1,0 +1,21 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    document_date = fields.Date()
+
+    @api.onchange("invoice_date")
+    def _onchange_invoice_date(self):
+        for record in self:
+            if not record.document_date:
+                record.document_date = record.invoice_date
+
+    def action_post(self):
+        res = super().action_post()
+        if not self.document_date:
+            self.document_date = self.invoice_date
+        return res

account_analytic_document_date/models/account_move_line.py
@@ -1,0 +1,16 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    def _prepare_analytic_lines(self):
+        vals = super()._prepare_analytic_lines()
+        for val in vals:
+            if self.move_id.document_date:
+                val.update({"document_date": self.move_id.document_date})
+            else:
+                val.update({"document_date": self.move_id.invoice_date})
+        return vals

account_analytic_document_date/tests/test_document_date.py
@@ -1,0 +1,101 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from datetime import datetime, timedelta
+
+from odoo.tests import TransactionCase
+
+
+class TestAccountMoveDocumentDate(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestAccountMoveDocumentDate, cls).setUpClass()
+        cls.account_move_model = cls.env["account.move"]
+
+        cls.partner = cls.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+                "email": "test@partner.com",
+                "phone": "123456789",
+            }
+        )
+
+        cls.analytic_plan_1 = cls.env["account.analytic.plan"].create(
+            {
+                "name": "Plan 1",
+                "default_applicability": "unavailable",
+                "company_id": False,
+            }
+        )
+
+        cls.analytic_account_1 = cls.env["account.analytic.account"].create(
+            {"name": "Account 1", "plan_id": cls.analytic_plan_1.id}
+        )
+
+    def test_create_invoice_without_document_date(self):
+        invoice = self.account_move_model.create(
+            {
+                "move_type": "out_invoice",
+                "partner_id": self.partner.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test Product",
+                            "quantity": 1,
+                            "price_unit": 100.0,
+                            "tax_ids": [
+                                (6, 0, [self.env["account.tax"].search([], limit=1).id])
+                            ],
+                            "analytic_distribution": {self.analytic_account_1.id: 100},
+                        },
+                    )
+                ],
+            }
+        )
+
+        invoice.action_post()
+
+        self.assertEqual(invoice.document_date, invoice.invoice_date)
+
+        # Confirms that the analytic line has the same document_date as the account.move
+        analytic_line = self.env["account.analytic.line"].search(
+            [("move_line_id", "in", invoice.line_ids.ids)], limit=1
+        )
+        self.assertEqual(invoice.document_date, analytic_line.document_date)
+
+    def test_create_invoice_with_document_date(self):
+        document_date = datetime.now().date() + timedelta(days=1)
+        invoice = self.account_move_model.create(
+            {
+                "move_type": "out_invoice",
+                "partner_id": self.partner.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test Product",
+                            "quantity": 1,
+                            "price_unit": 100.0,
+                            "tax_ids": [
+                                (6, 0, [self.env["account.tax"].search([], limit=1).id])
+                            ],
+                            "analytic_distribution": {self.analytic_account_1.id: 100},
+                        },
+                    )
+                ],
+                "document_date": document_date,
+            }
+        )
+
+        invoice.action_post()
+
+        self.assertEqual(invoice.document_date, document_date)
+        self.assertNotEqual(invoice.invoice_date, invoice.document_date)
+
+        # Confirms that the analytic line has the same document_date as the account.move
+        analytic_line = self.env["account.analytic.line"].search(
+            [("move_line_id", "in", invoice.line_ids.ids)], limit=1
+        )
+        self.assertEqual(invoice.document_date, analytic_line.document_date)

adhoc-cicd/oca-account-analytic

account_analytic_document_date/hooks.py
@@ -1,0 +1,12 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+def post_init_hook(cr, registry):
+    cr.execute(
+        """
+        UPDATE account_move
+        SET analytic_document_date = invoice_date
+        WHERE analytic_document_date IS NULL
+    """
+    )

account_analytic_document_date/models/account_move.py
@@ -1,0 +1,22 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    analytic_document_date = fields.Date()
+
+    @api.onchange("invoice_date")
+    def _onchange_invoice_date(self):
+        for record in self:
+            if not record.analytic_document_date:
+                record.analytic_document_date = record.invoice_date
+
+    def action_post(self):
+        res = super().action_post()
+        for record in self:
+            if not record.analytic_document_date:
+                record.analytic_document_date = record.invoice_date
+        return res

account_analytic_document_date/models/account_move_line.py
@@ -1,0 +1,16 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    def _prepare_analytic_lines(self):
+        vals = super()._prepare_analytic_lines()
+        for val in vals:
+            if self.move_id.analytic_document_date:
+                val.update({"document_date": self.move_id.analytic_document_date})
+            else:
+                val.update({"document_date": self.move_id.invoice_date})
+        return vals

account_analytic_document_date/tests/test_document_date.py
@@ -1,0 +1,101 @@
+# Copyright 2024 (APSL - Nagarro) Miquel Pascual, Bernat Obrador
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from datetime import datetime, timedelta
+
+from odoo.tests import TransactionCase
+
+
+class TestAccountMoveDocumentDate(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestAccountMoveDocumentDate, cls).setUpClass()
+        cls.account_move_model = cls.env["account.move"]
+
+        cls.partner = cls.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+                "email": "test@partner.com",
+                "phone": "123456789",
+            }
+        )
+
+        cls.analytic_plan_1 = cls.env["account.analytic.plan"].create(
+            {
+                "name": "Plan 1",
+                "default_applicability": "unavailable",
+                "company_id": False,
+            }
+        )
+
+        cls.analytic_account_1 = cls.env["account.analytic.account"].create(
+            {"name": "Account 1", "plan_id": cls.analytic_plan_1.id}
+        )
+
+    def test_create_invoice_without_document_date(self):
+        invoice = self.account_move_model.create(
+            {
+                "move_type": "out_invoice",
+                "partner_id": self.partner.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test Product",
+                            "quantity": 1,
+                            "price_unit": 100.0,
+                            "tax_ids": [
+                                (6, 0, [self.env["account.tax"].search([], limit=1).id])
+                            ],
+                            "analytic_distribution": {self.analytic_account_1.id: 100},
+                        },
+                    )
+                ],
+            }
+        )
+
+        invoice.action_post()
+
+        self.assertEqual(invoice.analytic_document_date, invoice.invoice_date)
+
+        # Confirms that the analytic line has the same document_date as the account.move
+        analytic_line = self.env["account.analytic.line"].search(
+            [("move_line_id", "in", invoice.line_ids.ids)], limit=1
+        )
+        self.assertEqual(invoice.analytic_document_date, analytic_line.document_date)
+
+    def test_create_invoice_with_document_date(self):
+        document_date = datetime.now().date() + timedelta(days=1)
+        invoice = self.account_move_model.create(
+            {
+                "move_type": "out_invoice",
+                "partner_id": self.partner.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test Product",
+                            "quantity": 1,
+                            "price_unit": 100.0,
+                            "tax_ids": [
+                                (6, 0, [self.env["account.tax"].search([], limit=1).id])
+                            ],
+                            "analytic_distribution": {self.analytic_account_1.id: 100},
+                        },
+                    )
+                ],
+                "analytic_document_date": document_date,
+            }
+        )
+
+        invoice.action_post()
+
+        self.assertEqual(invoice.analytic_document_date, document_date)
+        self.assertNotEqual(invoice.invoice_date, invoice.analytic_document_date)
+
+        # Confirms that the analytic line has the same document_date as the account.move
+        analytic_line = self.env["account.analytic.line"].search(
+            [("move_line_id", "in", invoice.line_ids.ids)], limit=1
+        )
+        self.assertEqual(invoice.analytic_document_date, analytic_line.document_date)